00246deba7439448960675488ffbc4b894725179
Loxican <loxican@outlook.sg>
2024-02-22 16:06:50 +0000

    removed eruda console

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import eruda from "eruda";
 import "./style.css";
 
 import { Game } from "./Game";
-
-eruda.init();
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
     <React.StrictMode>
